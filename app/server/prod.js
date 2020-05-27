3716905924977a36a667a0be6d2c8368a3e28ebd
Sergi Chalauri <sergi@thedataincubator.com>
2020-05-27 06:55:55 +0000

    GAE deploy-ability

--- app/server/prod.js
+++ app/server/prod.js
@@ -2,7 +2,9 @@
 
 var app = require('./lib').default; // "src" babel code gets transpiled to "lib" directory
 
-const port = process.env.PORT || 3001;
+// It looks like that GAE is setting PORT env var (to 8080).
+// Let nginx listen to 8080 and forward requests to hard-coded 3001 port.
+const port = 3001;
 
 const server = app.listen(port);
 
