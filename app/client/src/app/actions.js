2d20ba34a81bdabda3da6b25c63c3abef5241d10
Sergi Chalauri <sergi@thedataincubator.com>
2020-06-10 07:25:26 +0000

    Simplify fetch call

--- app/client/src/app/actions.js
+++ app/client/src/app/actions.js
@@ -17,21 +17,12 @@ function initializeFellowJsonresume(fellowKeyUrlsafe: ?string): AsyncAction {
   return async (dispatch, getState) => {
     // TODO: redux-ize below action and integrate into the application
     const { fetch } = window
-    const request = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // body: JSON.stringify(resumeData),
-    }
-
     try {
-      fellowKeyUrlsafe
       const jsonresumeFetchBaseUrl = '/fellows/fetch_resume_json'
       const jsonresumeFetchUrl = fellowKeyUrlsafe ? jsonresumeFetchBaseUrl + '/' + fellowKeyUrlsafe : jsonresumeFetchBaseUrl
-      const response = await fetch(jsonresumeFetchUrl, request)
-      const responseText = await response.text()
-      alert(`Fetched jsonresume: ${responseText}`)
+      const response = await fetch(jsonresumeFetchUrl)
+      const fellowData = await response.json()
+      alert(`Fetched fellow data: ${JSON.stringify(fellowData)}`)
     } catch (err) {
       alert('errored out')
       console.log(err)
