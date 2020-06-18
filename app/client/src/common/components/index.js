a837e61c61e79bdd503286c12f6f0f1d337d9db7
Sergi Chalauri <sergi@thedataincubator.com>
2020-06-18 08:37:43 +0000

    rm TmpButton.js

--- app/client/src/common/components/index.js
+++ app/client/src/common/components/index.js
@@ -7,10 +7,9 @@ import Icon from './Icon'
 import Button, { MarginlessButton } from './Button'
 import PrimaryButton from './PrimaryButton'
 import RoundButton from './RoundButton'
-import TmpButton from './TmpButton'
 import ScrollToTop from './ScrollToTop'
 import Loader, { Bars } from './Loader'
 import Logo from './Logo'
 import Row from './Row'
 
-export { Button, MarginlessButton, PrimaryButton, RoundButton, TmpButton, Divider, Swap, Icon, ScrollToTop, Loader, Logo, Bars, Row }
+export { Button, MarginlessButton, PrimaryButton, RoundButton, Divider, Swap, Icon, ScrollToTop, Loader, Logo, Bars, Row }
