e4210810907a61c0eb87d0c2f1b22b574f426165
Sergi Chalauri <sergi@thedataincubator.com>
2020-07-16 10:01:39 +0000

    Don't allow deploying master from arbitrary branches

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,16 @@ function usage_and_exit_1 {
     exit 1
 }
 
+function check_production_branch {
+    local master_branch='newtemplate'
+    local git_branch=$(git symbolic-ref --short HEAD)
+    if test $master_branch != $git_branch
+    then
+        echo 'You must be on "'$master_branch'" branch to deploy production!' 1>&2
+        exit 1
+    fi
+}
+
 if test $# -ne 1
 then
     usage_and_exit_1
@@ -14,6 +24,8 @@ fi
 
 case $1 in
     'production')
+        echo -e '\033[0;31mDeploying to production!\033[0m'
+        check_production_branch
         PROJECT='thedataincubator'
         ;;
     'staging')
@@ -26,7 +38,7 @@ esac
 
 if test -n "$(git status --short)"
 then
-    echo 'There are some uncommitted changes!' 1>&2
+    echo 'There are some uncommitted changes, please commit them first!' 1>&2
     exit 1
 fi
 
